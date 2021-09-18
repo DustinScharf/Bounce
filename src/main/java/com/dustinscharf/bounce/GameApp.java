c241458c13575736e532db14980e083741493d2b
DustinScharf <d.scharf.2002@gmail.com>
2021-09-18 18:38:13 +0200

    Added empty initSettings methode

--- src/main/java/com/dustinscharf/bounce/GameApp.java
+++ src/main/java/com/dustinscharf/bounce/GameApp.java
@@ -1,7 +1,13 @@
 package com.dustinscharf.bounce;
 
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
 
 public class GameApp extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        // empty
+    }
+
     // TODO implemented basic window
 }
