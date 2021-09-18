444023cdd54cfbbffc701af04714c60c85f7a162
DustinScharf <d.scharf.2002@gmail.com>
2021-09-18 18:44:17 +0200

    Added empty initInput methode

--- src/main/java/com/dustinscharf/bounce/GameApp.java
+++ src/main/java/com/dustinscharf/bounce/GameApp.java
@@ -2,6 +2,10 @@ package com.dustinscharf.bounce;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import javafx.scene.input.KeyCode;
+
+import static com.almasb.fxgl.dsl.FXGL.getNotificationService;
+import static com.almasb.fxgl.dsl.FXGL.onKeyDown;
 
 public class GameApp extends GameApplication {
     @Override
@@ -12,6 +16,10 @@ public class GameApp extends GameApplication {
     @Override
     protected void initGame() {
         // empty
+    }
+
+    protected void initInput() {
+
     }
 
     public static void main(String[] args) {
